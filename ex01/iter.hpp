3db45ccb30e380d2136f0b5a21e9d4edec9e62d3
blatifat <blatifat@student.42heilbronn.de>
2024-10-17 04:53:14 +0200

    ex01

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: blatifat <blatifat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/06 02:18:39 by blatifat          #+#    #+#             */
-/*   Updated: 2024/10/06 02:20:22 by blatifat         ###   ########.fr       */
+/*   Updated: 2024/10/17 04:50:09 by blatifat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 #include <iostream>
 #include <string>
 
-// Fonction template iter qui applique une fonction sur chaque élément d'un tableau
 template <typename T, typename F>
 void iter(T* array, size_t length, F func) {
     for (size_t i = 0; i < length; ++i) {
